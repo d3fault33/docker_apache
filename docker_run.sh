895b079eff095c177b1ec07af6452ef9a4c3d066
Vadim Slepich <35778844+d3fault33@users.noreply.github.com>
2018-07-14 18:59:43 +0300

    Update docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -1,4 +1,6 @@
 #/bin/bash
 cd ~/container
+#building container py_apache with contents from working directory
 docker build -t py_apache .
+#running container in background exposing ports 80 and 4000
 docker RUN -d -p 80:80 -p 4000:4000 py_apache
